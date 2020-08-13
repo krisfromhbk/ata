2612724743a20264fa8fb3dcb63badcdf4d3c9c3
Kris <kreisnews@gmail.com>
2020-08-14 04:14:27 +1000

    Add test for select-based methods

--- internal/storage/store_test.go
+++ internal/storage/store_test.go
@@ -102,3 +102,103 @@ func TestCreateMessageBadAuthor(t *testing.T) {
 	_, err := s.CreateMessage(context.Background(), 4, 1, "Hi There!")
 	require.Equal(t, ErrMessageBadAuthor, err)
 }
+
+// TODO test not only by IDs but the whole chat rows
+func TestChatsByUserID(t *testing.T) {
+	s := bootstrap(t)
+	// number of users
+	n := 5
+
+	// generating n users in database
+	// test will retrieve chats for the first user
+	userIDs := make([]int64, n)
+	for i := range userIDs {
+		id, err := s.CreateUser(context.Background(), randString())
+		require.NoError(t, err)
+		userIDs[i] = id
+	}
+
+	// creating chats between users [0,1], [0,2], [0,3], etc.
+	chatIDs := make([]int64, n-1)
+	for i, v := range batchUserIDs(userIDs) {
+		id, err := s.CreateChat(context.Background(), randString(), v)
+		require.NoError(t, err)
+		chatIDs[i] = id
+	}
+
+	// creating 2 messages (author - first user) in each chat with 3 sec delay
+	for _, v := range chatIDs {
+		_, err := s.CreateMessage(context.Background(), v, userIDs[0], randString())
+		require.NoError(t, err)
+		time.Sleep(1 * time.Second)
+		_, err = s.CreateMessage(context.Background(), v, userIDs[0], randString())
+		require.NoError(t, err)
+	}
+
+	// retrieving chats by first userID
+	chats, err := s.ChatsByUserID(context.Background(), userIDs[0])
+	require.NoError(t, err)
+
+	expected := chatIDs
+	// reversing IDs in expected
+	for i := len(expected)/2 - 1; i >= 0; i-- {
+		opp := len(expected) - 1 - i
+		expected[i], expected[opp] = expected[opp], expected[i]
+	}
+
+	// extracting actual IDs
+	actual := make([]int64, 0, len(chats))
+	for _, v := range chats {
+		actual = append(actual, v.ID)
+	}
+
+	require.Equal(t, expected, actual)
+}
+
+func TestChatsByUserIDNotExist(t *testing.T) {
+	s := bootstrap(t)
+
+	_, err := s.ChatsByUserID(context.Background(), 0)
+	require.Equal(t, ErrUserNotExist, err)
+}
+
+// TODO test not only by IDs but the whole message rows
+func TestMessagesByChatID(t *testing.T) {
+	s := bootstrap(t)
+	// number of messages
+	n := 5
+
+	userOneID, err := s.CreateUser(context.Background(), randString())
+	require.NoError(t, err)
+	userTwoID, err := s.CreateUser(context.Background(), randString())
+	require.NoError(t, err)
+	chatID, err := s.CreateChat(context.Background(), randString(), []int64{userOneID, userTwoID})
+	require.NoError(t, err)
+
+	messageIDs := make([]int64, n)
+	for i := 0; i < n; i++ {
+		id, err := s.CreateMessage(context.Background(), chatID, userTwoID, randString())
+		require.NoError(t, err)
+		messageIDs[i] = id
+		require.NoError(t, err)
+	}
+
+	expected := messageIDs
+
+	messages, err := s.MessagesByChatID(context.Background(), chatID)
+	require.NoError(t, err)
+
+	actual := make([]int64, 0, len(messages))
+	for _, v := range messages {
+		actual = append(actual, v.ID)
+	}
+
+	require.Equal(t, expected, actual)
+}
+
+func TestMessagesByChatIDNotExist(t *testing.T) {
+	s := bootstrap(t)
+
+	_, err := s.MessagesByChatID(context.Background(), 0)
+	require.Equal(t, ErrChatNotExist, err)
+}
