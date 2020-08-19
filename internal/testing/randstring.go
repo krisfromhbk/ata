114f5205875a7ee7a5cd6af864519502fa131873
Kris <kreisnews@gmail.com>
2020-08-20 01:07:12 +1000

    Update RnadString func to generate globally unique strings

--- internal/testing/randstring.go
+++ internal/testing/randstring.go
@@ -1,19 +1,12 @@
 package testing
 
 import (
-	"math/rand"
-	"strings"
+	"github.com/rs/xid"
 )
 
 // RandString generates random string with 10 symbols length from lower- and uppercase alphabet
 func RandString() string {
-	var out strings.Builder
-	charSet := "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-	length := 10
-	for i := 0; i < length; i++ {
-		random := rand.Intn(len(charSet))
-		randomChar := charSet[random]
-		out.WriteString(string(randomChar))
-	}
-	return out.String()
+	guid := xid.New()
+
+	return guid.String()
 }
