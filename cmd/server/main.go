a3e6682de26c70c621ef8021ae4c47cc23a573d7
Kris <kreisnews@gmail.com>
2020-08-20 23:05:48 +1000

    Add main.go as proof of work to myself

---
+++ cmd/server/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"avito-trainee-assignment/internal/server"
+	"avito-trainee-assignment/internal/storage"
+	"go.uber.org/zap"
+	"log"
+)
+
+func main() {
+	logger, err := zap.NewDevelopment()
+	if err != nil {
+		log.Fatalf("zap.NewDevelopment: %v", err)
+	}
+	defer func() {
+		if err := logger.Sync(); err != nil {
+			log.Fatalf("logger.Sync: %v", err)
+		}
+	}()
+
+	sugar := logger.Sugar()
+	sugar.Info("Application is starting")
+
+	store, err := storage.NewStore(sugar, storage.TestConfig)
+	if err != nil {
+		sugar.Fatalf("Cannot create Store instance: %v", err)
+	}
+
+	srv, err := server.NewServer(sugar, server.Config{Port: 9000, Host: ""}, store)
+	if err != nil {
+		sugar.Fatalf("Cannot create Server instance: %v", err)
+	}
+
+	if err := srv.Start(); err != nil {
+		sugar.Fatalf("Cannot start http srv: %v", err)
+	}
+}
