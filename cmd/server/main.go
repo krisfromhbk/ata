7e3412e0c105dcfa2825266b85ab2e6f9bf4ebec
Kris <kreisnews@gmail.com>
2020-08-26 18:49:05 +0300

    Update main.go to use functional options to create Server and Store structs

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,8 +3,11 @@ package main
 import (
 	"avito-trainee-assignment/internal/server"
 	"avito-trainee-assignment/internal/storage"
+	"context"
+	"github.com/caarlos0/env/v6"
 	"go.uber.org/zap"
 	"log"
+	"time"
 )
 
 func main() {
@@ -17,12 +20,24 @@ func main() {
 	sugar := logger.Sugar()
 	sugar.Info("Application is starting")
 
-	store, err := storage.NewStore(sugar, storage.TestConfig)
+	sugar.Info("Current time:", time.Now())
+
+	cfg := server.EnvConfig{}
+	if err := env.Parse(&cfg); err != nil {
+		sugar.Fatalf("Cannot parse env config: %w", err)
+	}
+
+	store, err := storage.NewStore(context.Background(), sugar, storage.ConnectionTimeout(30*time.Second))
 	if err != nil {
 		sugar.Fatalf("Cannot create Store instance: %v", err)
 	}
 
-	srv, err := server.NewServer(sugar, server.Config{Port: 9000, Host: ""}, store)
+	serverOpts := []server.Option{
+		server.WithEnvConfig(cfg),
+		server.ReadTimeout(5 * time.Second),
+	}
+
+	srv, err := server.NewServer(sugar, store, serverOpts...)
 	if err != nil {
 		sugar.Fatalf("Cannot create Server instance: %v", err)
 	}
